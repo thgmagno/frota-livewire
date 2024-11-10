e61132f05deb497037d8bb9014dda3a30f7b7c83
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-11-10 19:36:08 -0300

    index testing

--- app/Livewire/Groups/Index.php
+++ app/Livewire/Groups/Index.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Groups;
 
+use App\Models\Group;
 use Livewire\Component;
 
 class Index extends Component
@@ -9,5 +10,10 @@ class Index extends Component
     public function render()
     {
         return view('livewire.groups.index');
+    }
+
+    public function getGroupsProperty()
+    {
+        return auth()->user()->groups;
     }
 }
--- tests/Feature/Livewire/Groups/IndexTest.php
+++ tests/Feature/Livewire/Groups/IndexTest.php
@@ -1,19 +1,27 @@
 <?php
 
-namespace Tests\Feature\Livewire\Groups;
-
 use App\Livewire\Groups\Index;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Livewire\Livewire;
-use Tests\TestCase;
+use App\Models\Group;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Livewire\livewire;
 
-class IndexTest extends TestCase
-{
-    /** @test */
-    public function renders_successfully()
-    {
-        Livewire::test(Index::class)
-            ->assertStatus(200);
-    }
-}
+beforeEach(function () {
+    $user = User::factory()->create();
+
+    actingAs($user);
+});
+
+test ('should list all the groups that belong to the user', function () {
+    $groupsThatBelongToUser = Group::factory()->count(3)->create([
+        'user_id' => auth()->id(),
+    ]);
+
+    $groupsThatDoNotBelongToUser = Group::factory()->count(3)->create();
+
+    livewire(Index::class)->assertSet('groups', function($groups) use($groupsThatBelongToUser, $groupsThatDoNotBelongToUser) {
+        return $groups->count() === $groupsThatBelongToUser->count()
+            && $groups->contains($groupsThatBelongToUser->first())
+            && !$groups->contains($groupsThatDoNotBelongToUser->first());
+    });
+});
